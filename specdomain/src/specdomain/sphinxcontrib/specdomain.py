7468c6a8f2f29afe1ae72ce84e9f350b801e2a49
jemian <jemian@2ae661ea-fc49-0410-bf30-f54ad2669282>
2012-06-12 22:03:27 +0000

    hitting a wall here, understanding how this is supposed to work

--- specdomain/src/specdomain/sphinxcontrib/specdomain.py
+++ specdomain/src/specdomain/sphinxcontrib/specdomain.py
@@ -30,10 +30,59 @@ from sphinx.util.nodes import make_refnode              #@UnusedImport
 from sphinx.util.docfields import Field, TypedField     #@UnusedImport
 
 
+class SpecObject(ObjectDescription):
+    """
+    Description of a SPEC object (macro definition or variable).
+    """
+    def _get_index_text(self, name):
+        if self.objtype == 'def':
+            return _('%s (SPEC macro)') % name
+        elif self.objtype == 'rdef':
+            return _('%s (SPEC macro)') % name
+        elif self.objtype == 'cdef':
+            return _('%s (SPEC global)') % name
+        else:
+            return ''
+
+
+class SpecXRefRole(XRefRole):
+    def process_link(self, env, refnode, has_explicit_title, title, target):
+        refnode['spec:def'] = env.temp_data.get('spec:def')
+        if not has_explicit_title:
+            title = title.lstrip(':')   # only has a meaning for the target
+            target = target.lstrip('~') # only has a meaning for the title
+            # if the first character is a tilde, don't display the module/class
+            # parts of the contents
+            if title[0:1] == '~':
+                title = title[1:]
+                colon = title.rfind(':')
+                if colon != -1:
+                    title = title[colon+1:]
+        return title, target
+
+
 class SpecDomain(Domain):
     """SPEC language domain."""
     name = 'spec'
     label = 'SPEC, http://www.certif.com'
+    object_types = {    # type of object that a domain can document
+        'def':  ObjType(l_('def'),  'def'),
+        'rdef': ObjType(l_('rdef'), 'rdef'),
+        'cdef': ObjType(l_('cdef'), 'cdef'),
+    }
+    directives = {
+        'def':          SpecObject,
+        'rdef':         SpecObject,
+        'cdef':         SpecObject,
+    }
+    roles = {
+        'def' :  SpecXRefRole(),
+        'rdef':  SpecXRefRole(),
+        'cdef':  SpecXRefRole(),
+    }
+    #indices = [
+    #    SpecMacroIndex,
+    #]
 
 
 # http://sphinx.pocoo.org/ext/tutorial.html#the-setup-function
